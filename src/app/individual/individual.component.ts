7f635faf56c7dcb4cd72ef6955d6244f34e4cd17
Albert Suarez <alsumo95@gmail.com>
2018-12-24 17:53:04 +0100

    :construction: Added interactionService for getting the json

--- src/app/individual/individual.component.ts
+++ src/app/individual/individual.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { InteractionService } from '../interaction.service';
+
 @Component({
   selector: 'app-individual',
   templateUrl: './individual.component.html',
-  styleUrls: ['./individual.component.scss']
+  styleUrls: ['./individual.component.scss'],
+  providers: [InteractionService]
 })
 export class IndividualComponent implements OnInit {
 
-  constructor() { }
+  constructor(private interactionService: InteractionService) { }
 
   ngOnInit() {
+    this.start();
+  }
+
+  private start() {
+    this.interactionService.getInteractionJson().subscribe((data) => console.log(data));
   }
 
 }
