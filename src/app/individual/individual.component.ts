6b8c0feac6a5f043559a2e31cfd7eed208ca52cf
Albert Suarez <alsumo95@gmail.com>
2018-12-27 20:58:32 +0100

    :construction: Got first video and buttons from JSON input

--- src/app/individual/individual.component.ts
+++ src/app/individual/individual.component.ts
@@ -20,7 +20,19 @@ export class IndividualComponent implements OnInit {
   }
 
   private start() {
-    this.interactionService.getInteractionJson().subscribe((data) => console.log(data));
+    this.interactionService.getInteractionJson().subscribe((data) => {
+      // Set video source
+      const initId = data.init;
+      $('#video-player').attr('src', 'http://localhost:8080/video?id=' + initId);
+      const videoPlayer: HTMLVideoElement = <HTMLVideoElement> $('#video-player')[0];
+      videoPlayer.load();
+
+      // Set buttons text
+      const initOptions = data[initId];
+      for (let i = 0; i < initOptions.length; i++) {
+        $('.container a').get(i).innerHTML = initOptions[i].value;
+      }
+    });
   }
 
   private onFinishVideo() {
