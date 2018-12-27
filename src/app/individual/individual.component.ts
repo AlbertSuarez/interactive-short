c5d0e60f863051614293d72d3e783ff07b0e658a
Albert Suarez <alsumo95@gmail.com>
2018-12-27 22:30:58 +0100

    :sparkles: Added first version of individual mode

--- src/app/individual/individual.component.ts
+++ src/app/individual/individual.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 
 import { InteractionService } from '../interaction.service';
+import * as config from '../config';
 
 
 @Component({
@@ -23,16 +24,29 @@ export class IndividualComponent implements OnInit {
     this.interactionService.getInteractionJson().subscribe((data) => {
       // Set video source
       const initId = data.init;
-      $('#video-player').attr('src', 'http://localhost:8080/video?id=' + initId);
+      $('#video-player').attr('src', config.serverUrl + initId);
       const videoPlayer: HTMLVideoElement = <HTMLVideoElement> $('#video-player')[0];
       videoPlayer.load();
 
       // Set buttons text
       const initOptions = data[initId];
       for (let i = 0; i < initOptions.length; i++) {
-        $('.container a').get(i).innerHTML = initOptions[i].value;
+        const buttonElement: HTMLAreaElement = <HTMLAreaElement> $('.container a').get(i);
+        buttonElement.innerHTML = initOptions[i].value;
+        buttonElement.addEventListener('click', (e: Event) => this.changeVideo(initOptions[i].key));
       }
     });
+  }
+
+  private changeVideo(key: string) {
+    $('.container').hide();
+
+    // Set video source
+    $('#video-player').attr('src', config.serverUrl + key);
+    const videoPlayer: HTMLVideoElement = <HTMLVideoElement> $('#video-player')[0];
+    videoPlayer.load();
+
+    return null;
   }
 
   private onFinishVideo() {
